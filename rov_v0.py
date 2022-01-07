a7943fc50988334dcb62dbfc7dcdbbe8d5934899
abcde3 <50956855+abcde3@users.noreply.github.com>
2022-01-07 16:20:39 +0800

    Add files via upload

--- rov_v0.py
+++ rov_v0.py
@@ -6,7 +6,7 @@ import json
 with open('config.json','r') as f:
     cfg = json.load(f)
 
-file_name = '20211130.txt' # 先手动输入
+file_name = '20211130.txt'
 #db = pymysql.connect(host=cfg['mysql_host'], port=cfg['mysql_port'], user=cfg['mysql_user'], passwd=cfg['mysql_passwd'], db=cfg['mysql_db2'])
 #db.close()
 
@@ -22,10 +22,16 @@ def build_tree(file_name):
             continue
         else:
             data = line.rstrip('\n').replace('\"','').split(',')
-            node = rtree.add(data[1])
-            node.data['asID'] = int(data[0])
-            node.data['maxLength'] = int(data[2])
-            node.data['root'] = data[3]
+            tmp = rtree.search_exact(data[1])
+            if tmp == None:
+                node = rtree.add(data[1])
+                node.data['asID'] = [int(data[0])]
+                node.data['maxLength'] = [int(data[2])]
+                node.data['root'] = [data[3]]
+            else:
+                tmp.data['asID'].append(int(data[0]))
+                tmp.data['maxLength'].append(int(data[2]))
+                tmp.data['root'].append(data[3])
             cnt += 1
     return rtree
 # print('hello')
@@ -40,21 +46,26 @@ def result_db_conn():
 def get_result(rtree,bgp_ip,bgp_as):
     node = rtree.search_best(bgp_ip)
     if node == None:
-        return 'not found'
+        return 'not found','not coverd by any node'
     else:
         max_length = int(bgp_ip.split('/')[1])
         if bgp_as.startswith('{') or bgp_as =='':
-            return 'not known'
-        if node.data['asID'] == int(bgp_as) and max_length<= node.data['maxLength']:
-            return 'valid'
+            return 'wrong data',''
+        #count = len(node.data['asID'])
+        #flag = False
+        if int(bgp_as) in node.data['asID']:
+            index = node.data['asID'].index(int(bgp_as))
+            if max_length <= node.data['maxLength'][index]:
+                return 'valid','Covered by '+node.prefix+' '+str(node.data['asID'][index])+'.Maxlength is '+str(node.data['maxLength'][index])
+            else:
+                return 'invalid','Covered by '+node.prefix+' '+str(node.data['asID'][index])+'.Maxlength is '+str(node.data['maxLength'][index])+'.But the length of announced prefix is illegal'
         else:
-            return 'invalid'
-
-
+            return 'invalid','The ASN is not the owner of the prefix '+bgp_ip+'.'
+        
 def prove(rtree):
     # rtree = build_tree(file_name)
     cnt = 0
-    insert_sql = "insert into result values(%s,%s,%s,%s,%s,%s) on duplicate key update " \
+    insert_sql = "insert into result2 values(%s,%s,%s,%s,%s,%s,%s,%s) on duplicate key update " \
                 "result=if(first>values(first),values(result),result)"
     conn1 = bgp_db_conn()
     conn2 = result_db_conn()
@@ -74,12 +85,10 @@ def prove(rtree):
             ip_type = row[2]
             first = row[6]
             last = row[7]
-            print(asID)
-            # print(ip_addr)
-            # print(asID)
-            # print(ip_type)
-            result = get_result(rtree,ip_addr,asID)
-            data = (ip_addr,asID,ip_type,result,first,last)
+            roa_file = ''
+            #print(asID)
+            result,reason = get_result(rtree,ip_addr,asID)
+            data = (ip_addr,asID,ip_type,result,reason,roa_file,first,last)
             data_l.append(data)
             cnt += 1
             count += 1
@@ -88,16 +97,10 @@ def prove(rtree):
             try:
                 result_cursor.executemany(insert_sql, data_l)
                 conn2.commit()
-                cnt =0
+                cnt =0 
                 data_l = []
             except Exception as err:
                 print(err)
-    # if cnt > 0:
-    #         try:
-    #             result_cursor.executemany(insert_sql, data_l)
-    #             conn2.commit()
-    #         except Exception as err:
-    #             print(err)
     result_cursor.close()
     cursor.close()
     conn1.close()
@@ -109,4 +112,3 @@ rtree = build_tree(file_name)
 prove(rtree)
 end = time.time()
 print(end -begin)
-# print(rtree.search_best('82.151.32.0/10'))
